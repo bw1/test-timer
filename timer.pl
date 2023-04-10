75d0e98d7b34a743aa903eae39be7f50267077ad
bw1 <bw1@aol.at>
2023-04-10 13:02:21 +0200

    count

--- timer.pl
+++ timer.pl
@@ -4,13 +4,17 @@
 #
 use strict;
 
+my $count=1;
+
 local $SIG{ALRM} = sub {
-	die "alarm\n";
+	print "$count alarm\n";
+	exit(0);
 };
 
 alarm 10;
 
 while ( 1==1 ) {
 	sleep 1;
-	print "run\n";
+	print "$count run\n";
+	$count++;
 }
